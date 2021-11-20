5e631d57a820d4a8c15692fc37a2d457b3d78ee9
Alex Mendoza <alexrmendoza@gmail.com>
2021-11-20 16:20:39 -0600

    created Day 1 project - Band Name Generator

---
+++ BandNameGenerator.py
@@ -0,0 +1,7 @@
+# Band Name Generator
+
+firstName = raw_input("What is your first name?")
+print(firstName)
+petName = raw_input("What is your pets name?")
+print(petName)
+print('Your band name could be ' + firstName + " " + petName)
