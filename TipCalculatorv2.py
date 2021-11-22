bd044e06ecc1f0f9bf79d843d0e72025afc1e408
Alex Mendoza <alexrmendoza@gmail.com>
2021-11-21 18:17:39 -0600

    tip calc og removed print statement; tip calc v2

--- TipCalculatorv2.py
+++ TipCalculatorv2.py
@@ -1,19 +1,19 @@
-# Tip Calculator
+#Tip Calculator v2
 
 print("Welcome to the tip calculator!")
 bill = float(raw_input("What was the total bill? $"))
 tip = int(raw_input("How much tip would you like to give? 10, 12, or 15? "))
 people = int(raw_input("How many people to split the bill? "))
 
-
-tip_percent = float(tip) / 100
-#print(tip_percent)
-total_tip = bill * tip_percent
-total_bill = bill + total_tip
-bill_per_person = total_bill / people
+bill_with_tip = bill * (1 + float(tip) / 100)
+# print(bill_with_tip)
+# tip_percent = float(tip) / 100
+# print(tip_percent)
+# total_tip = bill * tip_percent
+# total_bill = bill + total_tip
+bill_per_person = bill_with_tip / people
 
 final_bill = round(bill_per_person, 2)
 
 print("Each person should pay: $" + str(final_bill))
-
 
