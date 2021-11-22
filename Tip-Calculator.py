0d7060d2a564b7a6fc138d9a1c5d8287d659ce3c
Alex Mendoza <alexrmendoza@gmail.com>
2021-11-22 17:59:37 -0600

    changed version of python on VS Code, updated code

--- Tip-Calculator.py
+++ Tip-Calculator.py
@@ -1,13 +1,13 @@
 # Tip Calculator
 
 print("Welcome to the tip calculator!")
-bill = float(raw_input("What was the total bill? $"))
-tip = int(raw_input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(raw_input("How many people to split the bill? "))
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
 
 
-tip_percent = float(tip) / 100
-#print(tip_percent)
+tip_percent = tip / 100
+print(tip_percent)
 total_tip = bill * tip_percent
 total_bill = bill + total_tip
 bill_per_person = total_bill / people
--- TipCalculatorv2.py
+++ TipCalculatorv2.py
@@ -1,9 +1,9 @@
 #Tip Calculator v2
 
 print("Welcome to the tip calculator!")
-bill = float(raw_input("What was the total bill? $"))
-tip = int(raw_input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(raw_input("How many people to split the bill? "))
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
 
 bill_with_tip = bill * (1 + float(tip) / 100)
 # print(bill_with_tip)
