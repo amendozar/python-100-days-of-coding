b613c373e570438649e0633901242995b842e8df
Alex Mendoza <alexrmendoza@gmail.com>
2021-11-21 18:06:58 -0600

    if statement was not working correctly, removed it

--- Tip-Calculator.py
+++ Tip-Calculator.py
@@ -3,17 +3,11 @@
 print("Welcome to the tip calculator!")
 bill = float(raw_input("What was the total bill? $"))
 tip = int(raw_input("How much tip would you like to give? 10, 12, or 15? "))
-
-# require user to enter 10, 12, or 15
-if tip != 10 or tip != 12 or tip != 15:
-    tip = int(raw_input("Please enter 10, 12, or 15? "))
-else:
-    pass
-
 people = int(raw_input("How many people to split the bill? "))
 
 
 tip_percent = float(tip) / 100
+print(tip_percent)
 total_tip = bill * tip_percent
 total_bill = bill + total_tip
 bill_per_person = total_bill / people
