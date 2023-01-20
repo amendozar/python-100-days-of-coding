b52c7b72b475fedd138a2603f04c35cd2c48db3c
Alex Mendoza <alexrmendoza@gmail.com>
2023-01-19 19:49:50 -0600

    func to select rand acct, working on if else

--- Higher_Lower_Game.py
+++ Higher_Lower_Game.py
@@ -322,38 +322,61 @@ data = [
 ]
 
 import random as rd
+
 # function to select random accoints
+def select_rand():
+    length_data = len(data)
+    select_random = data[rd.randint(1,length_data+1)]
 
-length_data = len(data)
+    return select_random
 
-select_random_A = data[rd.randint(1,length_data+1)]
-select_random_B = data[rd.randint(1,length_data+1)]
+a = select_rand()
+b = select_rand()
+
+##### Swap function?
+# if guess == "A" and a['follower_count'] > b['follower_count']:
+    #then select new A new_A = data[rd.randint(1,length_data+1)]
+
+# elif guess == "B" and a['follower_count'] < b['follower_count']:
+    #B becomes A, select new B
 
 
 # present 2 random instagram accounts A & B
 # print logo
 print(logo)
 # print first option with info
-print('Compare A: ', select_random_A['name'], 'a ',select_random_A['description'], 'from ',select_random_A['country'])
+print('Compare A: ', a['name'], 'a ',a['description'], 'from ',a['country'], a['follower_count'])
 # print vs 
 print(vs)
 # print second option with info
-print('Compare B: ', select_random_A['name'], 'a ',select_random_A['description'], 'from ',select_random_A['country'])
+print('Compare B: ', b['name'], 'a ',b['description'], 'from ',b['country'], b['follower_count'])
 
 # ask which has more followers A or B
 guess = input("Who has more followers? Type 'A' or 'B': ")
 
 # count of correct guesses
 right_answers = 0
-# if guess == "A" and a_followerCount > b_followerCount:
-    # then select new A new_A = data[rd.randint(1,length_data+1)]
-    # right_answers = right_answers + 1
-# elif guess == "B" and a_followerCount < b_followerCount:
-    # B becomes A, select new B
-    # right_answers = right_answers + 1
-# else:
-# print("You Lose! Loser!!!!!!!")
+if guess == "A" and a['follower_count'] > b['follower_count']:
+    #then select new A new_A = data[rd.randint(1,length_data+1)]
+    b = a
+    a = select_rand()
+    right_answers = right_answers + 1
+    print("A was right!!!")
+    print(right_answers)
+elif guess == "B" and a['follower_count'] < b['follower_count']:
+    #B becomes A, select new B
+    a = b
+    b = select_rand()
+    right_answers = right_answers + 1
+    print("B was right!!!")
+    print(right_answers)
+else:
+    print("You Lose! Loser!!!!!!!")
 
+print(1)
+print(a)
+print(2)
+print(b)
 
 # ask which has more followers A or B
 # if guess is correct, add tally:
